5d7351b472bec7d521700fe4f4ef15402eb2725b
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2019-02-14 18:48:41 +0000

    Add save to C/asm options

--- src/main/java/tilesheet/GuiFrame.java
+++ src/main/java/tilesheet/GuiFrame.java
@@ -4,8 +4,16 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
+import java.awt.image.DataBufferShort;
+import java.awt.image.DataBufferUShort;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
@@ -62,12 +70,39 @@ public class GuiFrame extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser chooser = new JFileChooser(file);
+
                 chooser.setFileFilter(new FileNameExtensionFilter("PNG image files", "png"));
+                chooser.addChoosableFileFilter(new FileNameExtensionFilter("C source code", "c"));
+                chooser.addChoosableFileFilter(new FileNameExtensionFilter("AVR assembly source code", "asm"));
+
                 chooser.setAcceptAllFileFilterUsed(false);
 
                 if(chooser.showSaveDialog(GuiFrame.this) == JFileChooser.APPROVE_OPTION) {
+                    File file = chooser.getSelectedFile();
+                    String ext;
+
+                    FileNameExtensionFilter filter = (FileNameExtensionFilter)chooser.getFileFilter();
+                    if(!filter.accept(file)) {
+                        ext = filter.getExtensions()[0];
+                        file = new File(file.getAbsolutePath() + '.' + ext);
+                    } else {
+                        ext = getExtension(file);
+                    }
+
+                    String cleanName = GuiFrame.this.file.getName().replaceAll("[^a-zA-Z]", "");
+                    BufferedImage stitched = TilesheetConverter.stitch(context, image);
+
                     try {
-                        ImageIO.write(TilesheetConverter.stitch(context, image), "png", chooser.getSelectedFile());
+                        if(ext.equals("png")) {
+                            ImageIO.write(stitched, "png", file);
+                        } else {
+                            byte[] data = getRawBytes(stitched.getRaster().getDataBuffer(), ByteOrder.BIG_ENDIAN);
+                            String source = ext.equals("asm") ? HexUtil.getAvrDb(cleanName, data, 16) : HexUtil.getCArray(cleanName, data, 16);
+
+                            try(PrintWriter writer = new PrintWriter(file)) {
+                                writer.write(source);
+                            }
+                        }
                     } catch(IOException ex) {
                         ex.printStackTrace();
                         JOptionPane.showMessageDialog(GuiFrame.this, String.format("Could not save image.%n%s", ex.getMessage()), "Save failed", JOptionPane.ERROR_MESSAGE);
@@ -84,6 +119,16 @@ public class GuiFrame extends JFrame {
         pack();
     }
 
+    /**
+     * Gets the file extension from a file, if any, without the dot.
+     * If a file has no extension this returns an empty string.
+     */
+    private static String getExtension(File file) {
+        String name = file.getName();
+        int i = name.lastIndexOf('.');
+        return name.substring(i == -1 ? name.length() : i+1);
+    }
+
     public void setImage(File file, BufferedImage image) {
         this.file = file;
         this.image = image;
@@ -94,5 +139,41 @@ public class GuiFrame extends JFrame {
         this.context = context;
         viewer.setContext(context);
         rightPanel.setContext(context);
+    }
+
+    private static byte[] getRawBytes(DataBuffer buffer, ByteOrder order) {
+        if(buffer instanceof DataBufferByte) {
+            return ((DataBufferByte)buffer).getData();
+        } else if(buffer instanceof DataBufferUShort) {
+            return shortsToBytes(((DataBufferUShort)buffer).getData(), order);
+        } else if(buffer instanceof DataBufferShort) {
+            return shortsToBytes(((DataBufferUShort)buffer).getData(), order);
+        } else if(buffer instanceof DataBufferInt) {
+            return intsToBytes(((DataBufferInt)buffer).getData(), order);
+        } else {
+            throw new IllegalArgumentException("Invalid data buffer type " + buffer.getClass());
+        }
+    }
+
+    private static byte[] shortsToBytes(short[] shorts, ByteOrder order) {
+        byte[] bytes = new byte[shorts.length * 2];
+
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        byteBuffer.order(order);
+        byteBuffer.asShortBuffer().put(shorts);
+        byteBuffer.flip();
+
+        return bytes;
+    }
+
+    private static byte[] intsToBytes(int[] ints, ByteOrder order) {
+        byte[] bytes = new byte[ints.length * 4];
+
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        byteBuffer.order(order);
+        byteBuffer.asIntBuffer().put(ints);
+        byteBuffer.flip();
+
+        return bytes;
     }
 }
